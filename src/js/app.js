90313d5df108465ced4def89ce5ac4c9f82b9859
wookkii <wookkii@home>
2018-09-18 17:21:59 +1000

    Transition collapsibles

--- src/js/app.js
+++ src/js/app.js
@@ -10,11 +10,27 @@ document.addEventListener('click', function (event) {
     // console.log(event.target);
     
     // Toggle class
-     var menuBgElements = document.getElementsByClassName('menu--bg')
+     var menuBgElements = document.getElementsByClassName('transition--bg1')
     Array.prototype.map.call(menuBgElements, function(el) {
-        return el.classList.toggle('menu--expanded');
+        return el.classList.toggle('expand');
     }
     );
+
+    
+
+}, false);
+
+document.addEventListener('click', function (event) {
+
+	// If the clicked element doesn't have the right selector, bail
+	if (!event.target.matches('.lbl-toggle')) return;
+
+	// Don't follow the link
+	// event.preventDefault();
+
+	// Log the clicked element in the console
+    console.log(event.target);
+
 
     
 
