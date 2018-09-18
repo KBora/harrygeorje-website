4764b0b42158f7163e65757ac1c28b23ce5f33c1
wookkii <wookkii@home>
2018-09-18 15:09:45 +1000

    Mobile menu fixed scrollable overlay

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('click', function (event) {
-    console.log(event.target);
+
 	// If the clicked element doesn't have the right selector, bail
 	if (!event.target.matches('#collapsibleMobileMenu')) return;
 
@@ -7,17 +7,15 @@ document.addEventListener('click', function (event) {
 	// event.preventDefault();
 
 	// Log the clicked element in the console
-    console.log(event.target);
+    // console.log(event.target);
     
     // Toggle class
      var menuBgElements = document.getElementsByClassName('menu--bg')
-    // if (menuBgElements) {
-    //     menuBgElements[0].classList.toggle('menu--expanded');
-    // }
-
     Array.prototype.map.call(menuBgElements, function(el) {
         return el.classList.toggle('menu--expanded');
     }
     );
+
+    
 
 }, false);
